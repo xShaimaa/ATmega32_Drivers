55491dbdc27c24c4bd5b166c30caf1a45382f7b0
Shaimaa Elbadrawy <54189670+xShaimaa@users.noreply.github.com>
2020-08-03 07:10:38 +0200

    Update STD_TYPES.h

--- STD_TYPES.h
+++ STD_TYPES.h
@@ -9,18 +9,18 @@
 #define STD_TYPES_H_
 
 //unsigned data types
-typedef unsigned char		u8_t ;
+typedef unsigned char		    u8_t ;
 typedef unsigned short int  u16_t;
 typedef unsigned long int 	u32_t;
 typedef unsigned long long  u64_t;
 
 //signed data types
-typedef signed char			s8_t ;
+typedef signed char			    s8_t ;
 typedef signed short int  	s16_t;
-typedef signed long int 	s32_t;
-typedef signed long long 	s64_t;
+typedef signed long int 	  s32_t;
+typedef signed long long 	  s64_t;
 
-typedef float  				f32_t;
-typedef double 				f64_t;
+typedef float  				      f32_t;
+typedef double 				      f64_t;
 
 #endif
