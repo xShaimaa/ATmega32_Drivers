9afd59a718e61d4534d7d2dcc4f1ce6091575c94
xShaimaa <xshimoo@gmail.com>
2020-09-12 20:08:24 +0200

    GPIO Driver Updated

---
+++ GPIO/GPIO.c
@@ -0,0 +1,177 @@
+/*
+ * GPIO.c
+ *
+ * Created: 03-Aug-20
+ *  Author: shimo
+ */
+ 
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+#include "GPIO.h"
+
+
+void GPIO_voidSetPinDir(u8_t port_name, u8_t pin_num, u8_t status)
+{
+	switch (port_name){
+		case PORTA:
+			if (status == OUTPUT){
+				SET_BIT(GPIO_DDRA, pin_num);
+			}
+			
+			else if (status == INPUT) {
+				CLR_BIT(GPIO_DDRA, pin_num);
+			}
+			break;
+		
+		case PORTB:
+			if (status == OUTPUT){
+				SET_BIT(GPIO_DDRB, pin_num);
+			}
+			
+			else if (status == INPUT) {
+				CLR_BIT(GPIO_DDRB, pin_num);
+			}
+			break;
+		
+		case PORTC:
+			if (status == OUTPUT){
+				SET_BIT(GPIO_DDRC, pin_num);
+			}
+		
+			else if (status == INPUT) {
+				CLR_BIT(GPIO_DDRC, pin_num);
+			}
+			break;
+		
+		case PORTD:
+			if (status == OUTPUT){
+				SET_BIT(GPIO_DDRD, pin_num);
+			}
+		
+			else if (status == INPUT) {
+				CLR_BIT(GPIO_DDRD, pin_num);
+			}
+			break;
+	}
+}
+
+
+void GPIO_voidSetPinVal(u8_t port_name, u8_t pin_num, u8_t val)
+{
+	switch (port_name){
+		case PORTA:
+			if (val == HIGH){
+				SET_BIT(GPIO_PORTA, pin_num);
+			} 
+		
+			else if (val == LOW){
+				CLR_BIT(GPIO_PORTA, pin_num);
+			}
+			break; 	
+		
+		case PORTB:
+			if (val == HIGH){
+				SET_BIT(GPIO_PORTB, pin_num);
+			}
+			
+			else if (val == LOW){
+				CLR_BIT(GPIO_PORTB, pin_num);
+			}
+			break;	
+		
+		case PORTC:
+			if (val == HIGH){
+				SET_BIT(GPIO_PORTC, pin_num);
+			}
+			
+			else if (val == LOW){
+				CLR_BIT(GPIO_PORTC, pin_num);
+			}
+			break;	
+		
+		case PORTD:
+			if (val == HIGH){
+				SET_BIT(GPIO_PORTD, pin_num);
+			}
+			
+			else if (val == LOW){
+				CLR_BIT(GPIO_PORTD, pin_num);
+			}
+			break;	
+	}
+}
+
+
+u8_t GPIO_u8ReadPin(u8_t port_name, u8_t pin_num)
+{
+	switch (port_name){
+		case PORTA:
+			return GET_BIT(GPIO_PINA, pin_num);	break;
+		
+		case PORTB:
+			return GET_BIT(GPIO_PINB, pin_num);	break;
+		
+		case PORTC:
+			return GET_BIT(GPIO_PINC, pin_num);	break;
+		
+		case PORTD:
+			return GET_BIT(GPIO_PIND, pin_num);	break;
+	}
+	return 0;
+}
+
+
+void GPIO_voidSetPortDir(u8_t port_name, u8_t status)
+{
+	switch (port_name){
+		case PORTA:
+			GPIO_DDRA = status;	break;
+		
+		case PORTB:
+			GPIO_DDRB = status;	break;
+		
+		case PORTC:
+			GPIO_DDRC = status;	break;
+		
+		case PORTD:
+			GPIO_DDRD = status;	break;
+	}
+}
+
+void GPIO_voidSetPortVal(u8_t port_name, u8_t val)
+{
+	switch (port_name){
+		case PORTA:
+			GPIO_PORTA = val;	break;
+		
+		case PORTB:
+			GPIO_PORTB = val;	break;
+		
+		case PORTC:
+			GPIO_PORTC = val;	break;
+		
+		case PORTD:
+			GPIO_PORTD = val;	break;
+	}
+}
+
+u8_t GPIO_u8ReadPort(u8_t port_name)
+{
+	switch (port_name){
+		case PORTA:
+			return GPIO_PINA;	break;
+		
+		case PORTB:
+			return GPIO_PINB;	break;
+		
+		case PORTC:
+			return GPIO_PINC;	break;
+		
+		case PORTD:
+			return GPIO_PIND;	break;
+	}
+	return 0;
+}
+
---
+++ GPIO/GPIO.h
@@ -0,0 +1,105 @@
+/*
+ * GPIO.h
+ *
+ * Created: 03-Aug-20
+ *  Author: shimo
+ */
+ 
+#ifndef GPIO_H_
+#define GPIO_H_
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+/* register definitions */
+
+/*PORTA registers */
+#define GPIO_PORTA *((volatile u8_t* const) 0x3B)
+#define GPIO_DDRA  *((volatile u8_t* const) 0x3A)
+#define GPIO_PINA  *((volatile u8_t* const) 0x39)
+
+/*PORTB registers */
+#define GPIO_PORTB *((volatile u8_t* const) 0x38)
+#define GPIO_DDRB  *((volatile u8_t* const) 0x37)
+#define GPIO_PINB  *((volatile u8_t* const) 0x36)
+
+/*PORTC regiters */
+#define GPIO_PORTC *((volatile u8_t* const) 0x35)
+#define GPIO_DDRC  *((volatile u8_t* const) 0x34)
+#define GPIO_PINC  *((volatile u8_t* const) 0x33)
+
+/*PORTD registers */
+#define GPIO_PORTD *((volatile u8_t* const) 0x32)
+#define GPIO_DDRD  *((volatile u8_t* const) 0x31)
+#define GPIO_PIND  *((volatile u8_t* const) 0x30)
+
+/* GPIO PORTs */
+#define PORTA 0
+#define PORTB 1
+#define PORTC 2
+#define PORTD 3
+
+/* GPIO PINs  */
+#define PIN0   0
+#define PIN1   1
+#define PIN2   2
+#define PIN3   3
+#define PIN4   4
+#define PIN5   5
+#define PIN6   6
+#define PIN7   7
+#define PIN8   8
+
+/* GPIO Dir */
+#define OUTPUT 1
+#define INPUT 0
+
+/* GPIO Vals*/
+#define HIGH 1
+#define LOW  0
+
+/* public functions prototypes */
+
+/******************************************************************************
+* Description : setting the PIN Direction
+* Parameters  : PORT (PORTA -> D) - PIN (PIN0 -> 8) - Status(INPUT - OUTPUT)
+* Return type : void
+******************************************************************************/
+void GPIO_voidSetPinDir(u8_t port_name, u8_t pin_num, u8_t status);
+
+/******************************************************************************
+* Description : setting the PIN Value
+* Parameters  : PORT (PORTA -> D) - PIN (PIN0 -> 8) - Val(HIGH - LOW)
+* Return type : void
+******************************************************************************/
+void GPIO_voidSetPinVal(u8_t port_name, u8_t pin_num, u8_t val);
+
+/******************************************************************************
+* Description : reading the PIN value
+* Parameters  : PORT (PORTA -> D) - PIN (PIN0 -> 8)
+* Return type : u8(pin value)
+******************************************************************************/
+u8_t GPIO_u8ReadPin(u8_t port_name, u8_t pin_num);
+
+/******************************************************************************
+* Description : setting the PORT Direction
+* Parameters  : PORT (PORTA -> D) - Status(INPUT - OUTPUT)
+* Return type : void
+******************************************************************************/
+void GPIO_voidSetPortDir(u8_t port_name, u8_t status);
+
+/******************************************************************************
+* Description : setting the PORT Value
+* Parameters  : PORT (PORTA -> D) - Val(HIGH - LOW)
+* Return type : void
+******************************************************************************/
+void GPIO_voidSetPortVal(u8_t port_name, u8_t val);
+
+/******************************************************************************
+* Description : reading the PORT value
+* Parameters  : PORT (PORTA -> D)
+* Return type : u8(pin value)
+******************************************************************************/
+u8_t GPIO_u8ReadPort(u8_t port_name);
+
+#endif
